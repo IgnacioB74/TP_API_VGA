cc1f5be6364e48d42dd27f168a7fcb0dd8f9326a
Ignacio Blanco <ignacioeblanco@gmail.com>
2026-06-08 21:12:03 -0300

    Primera Prueba para AzureKey

--- TP_API_VGA/Controllers/WeatherForecastController.cs
+++ TP_API_VGA/Controllers/WeatherForecastController.cs
@@ -12,22 +12,18 @@ namespace TP_API_VGA.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return _configuration["Key"] ?? "Llave NO encontrada o INVALIDA";
         }
     }
 }
